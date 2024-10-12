036d6b109951b1cd18977ada2fa346bc2b78473e
hxh19950701 <hxh19950701@qq.com>
2024-10-12 23:12:59 +0800

    更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vasthread.webviewtv"
         minSdk = 21
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
