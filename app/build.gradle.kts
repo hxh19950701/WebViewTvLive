49a96d0c257c210b49ef299edc775a279ea3c523
hxh <hxh@dct.com>
2024-11-15 14:53:31 +0800

    更新频道列表

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,4 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
     implementation("org.apache.commons:commons-lang3:3.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
