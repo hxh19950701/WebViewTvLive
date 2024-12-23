95b9fdf65125106a86448c74eb865b1b75b47dd0
hxh <hxh@dct.com>
2024-12-23 10:53:25 +0800

    更新频道

--- app/src/main/java/com/vasthread/webviewtv/playlist/PlaylistManager.kt
+++ app/src/main/java/com/vasthread/webviewtv/playlist/PlaylistManager.kt
@@ -29,8 +29,7 @@ object PlaylistManager {
     private val jsonTypeToken = object : TypeToken<List<Channel>>() {}
     private val playlistFile = File(application.filesDir, "playlist.json")
     private val builtInPlaylists = listOf(
-        Pair("中央&湖南", "https://raw.githubusercontent.com/hxh19950701/WebViewTvLive/main/app/channels/2.0/%E5%A4%AE%E8%A7%86%26%E6%B9%96%E5%8D%97.json"),
-        Pair("完整", "https://raw.githubusercontent.com/hxh19950701/WebViewTvLive/main/app/channels/2.0/%E5%AE%8C%E6%95%B4.json"),
+        Pair("默认", "https://raw.githubusercontent.com/hxh19950701/WebViewTvLive/main/channels.json"),
     )
 
     var onPlaylistChange: ((Playlist) -> Unit)? = null
@@ -103,7 +102,7 @@ object PlaylistManager {
     }
 
     private fun loadBuiltInPlaylist(): Playlist {
-        val json = application.assets.open("default_playlist.json").bufferedReader().use(BufferedReader::readText)
+        val json = "[]"
         return createPlaylistFromJson(json)
     }
 
